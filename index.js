827c94a3792d38a518875eb62a5e7335e61c0d5a
Roman <kavoriya@gmail.com>
2024-03-11 20:35:26 +0500

    Build the pages, add routing

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('node:http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+   let path = '';
+   switch(req.url) {
+      case '/':
+         path += 'index.html';
+         break;
+      case '/about':
+         path += 'about.html';
+         break;
+      case '/contact-me':
+         path += 'contact-me.html';
+         break;
+      default:
+         path += '404.html';
+         break;
+
+   }
+
+  fs.readFile(path, (err, data) => {
+   if (err) {
+      console.log(err);
+   } else {
+      res.end(data);
+   }
+  })
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
